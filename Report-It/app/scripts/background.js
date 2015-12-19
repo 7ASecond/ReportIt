c172873fa2558c0676bdb5d0c47fd4705023e6fd
7ASecond-Net <admin@7asecond.net>
2015-12-19 13:15:14 +0000

    3.02.13 version

---
+++ Report-It/app/scripts/background.js
@@ -0,0 +1,177 @@
+﻿chrome.contextMenus.create({
+    "title": "ReportIt!",
+    "contexts": ["all"],
+    "onclick": onClickHandler
+});
+
+var ExtId = "";
+// Create the XHR object.
+function CreateCorsRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+
+
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-Type", "application/json");
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+
+    return xhr;
+}
+
+// Decide what has been clicked on.
+// Get information to send to the Report-It POST Only API
+// Add content to the database so that it is not displayed again to the EU.
+// Tell the page (tab) showing the content to remove the content
+// Send the report to the Report-It POST only API
+
+// Information that must always be sent.
+//      1) Extension ID
+//      2) Username or ""
+//      3) Page URL
+//      4) Type of Report (Image, Text, Video, Page, etc)
+//      5) followed by Report Type Spectific Information
+//          5.1) Images and Videos:
+//              Src Url
+//          5.2) Text:
+//              Text Content
+//          5.3) Webpage
+//              No Additional information
+// Format:
+//      "extId,UserName,PageUrl,ReportType,SrcUrl,LinkUrl,TextContent"
+// Examples:
+//  Report Image (no Username):
+//          "extId,,PageUrl,Image,SrcUrl,,"
+//  Report Page:
+//          "extId,Username,PageUrl,Page,,,"           
+function onClickHandler(info) {
+
+    var extId = GetExtensionId();           // This add on's Unique Id for this computer
+    var username = GetUsername();           // The Username of the EU if registered & Logged on
+    var pUrl = encodeURI(info.pageUrl);     // The Page containing the reported content
+                         
+    var sUrl = "";                          // The source Url of the content if applicable
+    var lUrl = "";                          // The link url of the content if applicable
+    var sText = "";                         // The Selected text content if applicable
+
+    // Are we reporting an Image?
+    if (info.mediaType === "image") {
+       
+        sUrl = encodeURI(info.srcUrl);
+        SendReport(extId, username, pUrl, sUrl, lUrl, sText);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { greeting: "Image; " + sUrl }, function (response) {
+                console.log(response.farewell);
+            });
+        });
+
+    }
+    else if (info.linkUrl) {
+      
+        lUrl = encodeURI(info.linkUrl);
+        SendReport(extId, username, pUrl, sUrl, lUrl, sText);
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { greeting: "Link; " + lUrl }, function (response) {
+                console.log(response.farewell);
+            });
+        });
+    }
+   else if(info.selectionText) {      
+       sText = info.selectionText;
+       SendReport(extId, username, pUrl, sUrl, lUrl, sText);
+       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+           chrome.tabs.sendMessage(tabs[0].id, { greeting: "Text; " + lUrl }, function (response) {
+               console.log(response.farewell);
+           });
+       });
+   } else {
+        SendReport(extId, username, pUrl, sUrl, lUrl, sText);
+       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+           chrome.tabs.sendMessage(tabs[0].id, { greeting: "Page; " + lUrl }, function (response) {
+               console.log(response.farewell);
+           });
+       });
+    }
+};
+
+function SendReport(extId, username, pUrl, sUrl, lUrl, sText) {
+    // Format:
+    //      "extId,UserName,PageUrl,ReportType,SrcUrl,LinkUrl,TextContent"
+    var report = extId + "," + username + "," + pUrl + "," + " " + "," + sUrl + "," + lUrl + "," + sText;  // The Report that will be sent to the POST only API
+
+    // Setup our POST Headers
+    var xhr = new CreateCorsRequest("POST", "http://reportitapi2.azurewebsites.net/API/ReportIt/");
+    // For local debugging
+    // var xhr = new CreateCorsRequest("POST", "http://localhost:3070/API/ReportIt/");
+
+    xhr.onload = function () {
+        var responseText = xhr.responseText;
+        console.log("Response Text: " + responseText);
+        // process the response.
+    };
+
+    xhr.onerror = function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { greeting: "Error" }, function (response) {
+                console.log(response.farewell);
+                console.log(tabs[0].id);
+            });
+        });
+    };
+
+    // Send the Report
+    xhr.send(JSON.stringify(report));
+
+    // Let the EU know that they have reported the content successfully
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { greeting: "Reported" }, function (response) {
+            console.log(response.farewell);
+            console.log(tabs[0].id);
+        });
+    });
+}
+
+// Get's the Unique Id of the extension for this machine.
+function GetExtensionId() {
+    return localStorage.getItem("ReportItExtId");
+}
+
+// If the User is registered AND logged in return their username so that they can maintain statistics.
+function GetUsername() {
+    return "SYSTEM";
+}
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install") {
+        console.log("This is a first install!");
+        ExtId = getRandomToken();
+        console.log("Extension Id = " + ExtId);
+        localStorage.setItem("ReportItExtId", ExtId);
+    } else if (details.reason === "update") {
+        var thisVersion = chrome.runtime.getManifest().version;
+        ExtId = GetExtensionId();
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+});
+
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = "";
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
+}
+
---
+++ Report-It/app/scripts/contentScript.js
@@ -0,0 +1,107 @@
+﻿chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+     
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+
+      dhtmlx.message.position = "bottom";
+
+
+      if (request.greeting === "Reported")       // Show the Reported Message to the EU
+          dhtmlx.message({
+              text: "Successfully Reported!",
+              type: "info"
+          });
+      else if (request.greeting === "Error")     // Show the error message to the EU
+          dhtmlx.message({
+              text: "Failed to Report this",
+              type: "error"
+          });
+          // ReSharper disable once PossiblyUnassignedProperty
+      else if (request.greeting.startsWith("Image")) {
+          ProcessImage(request.greeting);
+      }
+      else  if (request.greeting.startsWith("Link")) {
+          ProcessLink(request.greeting);
+      }
+      else if (request.greeting.startsWith("Text")) {
+          ProcessText(request.greeting);
+      }
+      else if (request.greeting.startsWith("Page")) {
+          ProcessPage(request.greeting);
+      }
+  });
+
+var ProcessImage = function (greeting) {
+
+    var url = greeting.split(';');
+
+    // Replace the src with the r500 file
+    var imgUrl = chrome.extension.getURL("images/r500.png");
+
+    var imgs = document.getElementsByTagName("img");
+    for (var idx = 0; idx < imgs.length; idx++) {
+        if (imgs[idx].src === url[1].trim()) {
+
+            var width = imgs[idx].width;
+            var height = imgs[idx].height;           
+            imgs[idx].src = imgUrl;  // Change the Image and set it's properties   
+            imgs[idx].width = width; // Set our image to the same size as the previous image
+            imgs[idx].height = height;
+
+
+          imgs[idx].alt = "Removed by 7ASecond.Net";
+            
+            var r = imgs[idx].parentElement;
+            if (imgs[idx].parentElement.localName === 'a') {
+                imgs[idx].parentElement.href = "http://7ASecond.Net";
+                imgs[idx].baseURI = "http://7ASecond.Net";
+                imgs[idx].host = "http://7ASecond.Net";
+                imgs[idx].hostname = "http://7ASecond.Net";
+                imgs[idx].parentElement.removeAttribute('rel');
+            }
+            imgs[idx].title = "Removed by 7ASecond.Net";
+            
+        }
+    }
+};
+
+
+var ProcessLink = function(greeting) {
+    var url = greeting.split(';');
+    var links = document.getElementsByTagName("a");
+    for (var idx = 0; idx < links.length; idx++) {
+        if (links[idx].href === url[1].trim()) {            
+            
+            //var nLink = GenerateNewLinkText(links[idx].text);
+            //links[idx].text = nLink;
+            links[idx].text = "[Removed By 7ASecond]";
+            links[idx].href = "http://7ASecond.Net";            
+        }
+    }
+};
+
+function pad(padding, str, padLeft) {
+    if (typeof str === 'undefined')
+        return padding;
+    if (padLeft) {
+        return (padding + str).slice(-pad.length);
+    } else {
+        return (str + padding).substring(0, padding.length);
+    }
+}
+
+var GenerateNewLinkText = function (txt) {
+    var replacementText = "[Removed]";
+    var padding = Array(txt.length - replacementText.length).join(" "); // make a string of 255 spaces        
+    return pad(padding, replacementText, false);
+}
+
+var ProcessText = function(greeting) {
+    
+}
+
+var ProcessPage = function (greeting) {
+
+}
